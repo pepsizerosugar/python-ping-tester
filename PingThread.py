9fa0d499595ba7f73646fa44ae4843d4915f3f4f
pepsizerosugar <juntiger53@gmail.com>
2022-02-18 14:36:30 +0900

    [pepsi-004] create button function, apply dark theme

--- PingThread.py
+++ PingThread.py
@@ -2,29 +2,39 @@ from PyQt5.QtCore import QThread, pyqtSignal
 
 
 class PingThread(QThread):
-    progress = pyqtSignal(list)
+    progress = pyqtSignal(int, list)
 
-    def __init__(self, ip):
+    def __init__(self, currentRow, name, ip, logger):
         QThread.__init__(self)
+        self.currentRow = currentRow
+        self.name = name
         self.ip = ip
-        self.isRun = False
+        self.logger = logger
 
     def run(self):
-        while self.isRun:
-            self.ping(self.ip)
+        self.msleep(50)
+        self.ping(self.ip)
 
     def ping(self, ip):
+        self.logger.info('Ping server: %s(%s)', self.name, self.ip)
+
         import subprocess
         import re
 
         try:
-            ping_response = subprocess.Popen("ping -n 3 -w 1000 " + str(ip), stdout=subprocess.PIPE, shell=True)
+            ping_response = subprocess.Popen("ping -n 4 -w 1000 " + str(ip), stdout=subprocess.PIPE, shell=True)
             stdout, stderr = ping_response.communicate()
             stdout = stdout.decode("cp949").split('\n')[-2].replace(" ", "")
             result = re.findall(r"\d{1,3}ms", stdout)
+
             if len(result) != 3:
                 result = ["Fail", "Fail", "Fail"]
-            self.progress.emit(result)
+
+            self.logger.info('Ping result: %s(%s) %s', self.name, self.ip, result)
+            self.progress.emit(self.currentRow, result)
         except Exception as e:
             print(e)
+            self.logger.error(e)
             return
+
+        self.quit()
